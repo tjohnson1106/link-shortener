4e53cc92ae8fb0e28f1ec3fed6c24ca3ce07c586
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-21 18:06:54 -0500

    changed link module to use hash id as primary key instead of integer

--- lib/shorten/links/link.ex
+++ lib/shorten/links/link.ex
@@ -1,9 +1,11 @@
 defmodule Shorten.Links.Link do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Shorten.Ecto.HashId
 
+  @primary_key {:hash, HashId, [autogenerate: true]}
+  @derive {Phoenix.Param, key: :hash}
   schema "links" do
-    field :hash, :string
     field :url, :string
 
     timestamps()
@@ -12,9 +14,7 @@ defmodule Shorten.Links.Link do
   @doc false
   def changeset(link, attrs) do
     link
-    |> cast(attrs, [:hash, :url])
-    |> validate_required([:hash, :url])
-    |> unique_constraint(:hash)
-    |> unique_constraint(:url)
+    |> cast(attrs, [:url])
+    |> validate_required([:url])
   end
 end
